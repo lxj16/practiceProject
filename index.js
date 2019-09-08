3c61a2bfcd57fff0285c6f509433bfc2532a70f4
lxj16 <lxjlee16@gmail.com>
2019-09-08 15:34:55 -0700

    changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ hi: "fku" });
+  res.send({ lol: "fku" });
 });
 
 const PORT = process.env.PORT || 5000;
