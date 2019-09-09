f48ed2aaf8e3ba55567dd9890a6216c2cef223cb
lxj16 <sodagreen16@yeah.net>
2019-09-09 16:46:06 -0700

    oauth added

--- config/key.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  googleClientID:
-    "453471275650-8ln9qtsb6320n2htgt098qpd1l4vblhu.apps.googleusercontent.com",
-  googleClientSecret: "3P3osZb6xpsCsZwTgpMHw6M9"
-};
--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,17 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20");
-const keys = require("../config/key");
+const mongoose = require("mongoose");
+const keys = require("../config/keys");
+
+const User = mongoose.model("users");
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => done(null, user));
+});
 
 passport.use(
   new GoogleStrategy(
@@ -10,9 +21,15 @@ passport.use(
       callbackURL: "/auth/google/callback"
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log("access token", accessToken);
-      console.log("refresh token", refreshToken);
-      console.log("profile", profile);
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          done(null, existingUser);
+        } else {
+          new User({ googleId: profile.id })
+            .save()
+            .then(user => done(null, user));
+        }
+      });
     }
   )
 );
